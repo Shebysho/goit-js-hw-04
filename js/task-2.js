ae9262f1714834587e2261ff1480f03089aac3e3
Aargo911 <jekaboy2019@gmail.com>
2024-09-14 19:25:28 +0300

    task-2

--- js/task-2.js
+++ js/task-2.js
@@ -1,8 +1,13 @@
 function calcAverageCalories(days) {
+  if (days.length === 0) {
+    return 0;
+  }
+
   const totalCalories = days.reduce((total, day) => total + day.calories, 0);
-  
+
   return totalCalories / days.length;
 }
+
 console.log(
   calcAverageCalories([
     { day: "monday", calories: 3010 },
@@ -11,9 +16,9 @@ console.log(
     { day: "thursday", calories: 2900 },
     { day: "friday", calories: 3450 },
     { day: "saturday", calories: 3280 },
-    { day: "sunday", calories: 3300 }
+    { day: "sunday", calories: 3300 },
   ])
-);
+); // 3180
 
 console.log(
   calcAverageCalories([
@@ -23,10 +28,9 @@ console.log(
     { day: "thursday", calories: 1900 },
     { day: "friday", calories: 2370 },
     { day: "saturday", calories: 2280 },
-    { day: "sunday", calories: 2610 }
+    { day: "sunday", calories: 2610 },
   ])
-);
+); // 2270
 
-console.log(
-  calcAverageCalories([])
-);
+console.log(calcAverageCalories([])); // 0
+
